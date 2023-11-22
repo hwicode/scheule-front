601593bcf922c474da12a2f74d7ea0ff79fd4d71
hwicode <khc3796@naver.com>
2023-11-22 12:14:42 +0900

    Footer 생성

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <Navbar/>
   <Calendar/>
+  <Footer/>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
 import Calendar from "./components/Calendar.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: 'App',
   components: {
     Navbar : Navbar,
     Calendar : Calendar,
+    Footer : Footer,
   },
   
 };
