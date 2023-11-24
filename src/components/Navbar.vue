07afa7ddd49ed68384aadc6e6a4fb0b0315e657c
hwicode <khc3796@naver.com>
2023-11-24 14:00:38 +0900

    Navbar에서 검색을 할 때, 검색할 대상을 정하는 기능 구현함

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,6 +6,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="#">Daily Schedule</a>
@@ -17,10 +18,22 @@
           <a class="nav-link" href="#">LogIn</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+
+      <form class="d-flex align-items-center" role="search">
+        <div class="dropdown mr-2">
+          <button class="btn btn-xs dropdown-toggle text-white" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{ searchTopic }}
+          </button>
+          <ul class="dropdown-menu dropdown-menu-xs">
+            <li v-for="(topic, index) in topics" :key="index" @click="changeTopic(index)" class="dropdown-item" style="cursor:pointer;">
+              {{ topic }}
+            </li>
+          </ul>
+        </div>
+        <input class="form-control me-2 " type="search" placeholder="태그 이름을 입력하세요." aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
+
     </div>
   </div>
 </nav>
@@ -30,9 +43,23 @@
 <script>
 export default {
   name: 'Navbar',
+  data() {
+    return {
+      topics: ['계획표', '메모'],
+      searchTopic: '계획표',
+    };
+  },
+  methods: {
+    changeTopic(index) {
+      this.searchTopic = this.topics[index];
+    },
+  },
 }
 </script>
 
 <style scoped>
-
+  .btn-xs {
+    padding: 0.25rem 0.75rem;
+    font-size: 1rem;
+  }
 </style>
