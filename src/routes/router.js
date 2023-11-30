6c9fd0b22cfe202171078add0f90f8f14af00f63
hwicode <khc3796@naver.com>
2023-11-30 15:08:23 +0900

    라우팅을 했을 때 화면이 가장 위로 올라가게 만듦

--- src/routes/router.js
+++ src/routes/router.js
@@ -22,6 +22,10 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior() {
+      // 항상 페이지 상단으로 스크롤
+      return { top: 0 };
+    },
 });
 
 export default router;
