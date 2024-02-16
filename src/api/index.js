acea8896b000a95a4c498a64d3c3f579ce87a95d
hwicode <khc3796@naver.com>
2024-02-16 18:40:57 +0900

    axios 인터셉터를 통해 api 요청에 Authorization 헤더를 추가함

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,87 @@
 import axios from 'axios';
+import router from '@/routes/router.js'
+import { Mutex, tryAcquire, E_ALREADY_LOCKED } from 'async-mutex';
+
+import { reissueAuthToken } from './sign-in.js';
+
+const globalMutex = new Mutex();
 
 function apiInstance() {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_BASE_URL,
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    withCredentials: true,
+  });
+
+  instance.interceptors.request.use((config) => {
+      const accessToken = sessionStorage.getItem('authToken');
+      config.headers.Authorization = `${accessToken}`;
+      return config;
+    }
+  );
+
+  instance.interceptors.response.use(
+    response => response,
+    async (error) => {
+      if (!sessionStorage.getItem('logIn')) {
+        return Promise.reject(error);
+      }
+      
+      if (error.response && error.response.status === 401) {
+        try {
+          await tryAcquire(globalMutex).runExclusive(async () => {
+            const response = await reissueAuthToken();
+            const token = response.headers.getAuthorization();
+            sessionStorage.setItem('authToken', token);
+          });
+        } catch (error) {
+          if (error === E_ALREADY_LOCKED) {
+            return Promise.reject(error);
+          }
+        }
+
+        router.go();
+      }
+
+      return Promise.reject(error);
+    }
+  );
+  return instance;
+}
+
+function loginRequestApiInstance() {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_BASE_URL,
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    withCredentials: true,
+  });
+
+  instance.interceptors.response.use(
+    response => {
+      sessionStorage.setItem('logIn', true);
+      return response;
+    },
+    error => {
+      return handleLoginError(error);
+    }
+  )
+  return instance;
+}
+
+function handleLoginError(error) {
+  if (error.response) {
+    sessionStorage.removeItem('logIn');
+    sessionStorage.removeItem('authToken');
+    router.push('/');
+  }
+  return Promise.reject(error);
+}
+
+function getApiInstance() {
   const instance = axios.create({
     baseURL: process.env.VUE_APP_API_BASE_URL,
     headers: {
@@ -10,4 +91,4 @@ function apiInstance() {
   return instance;
 }
 
-export { apiInstance };
+export { apiInstance, loginRequestApiInstance, getApiInstance };
--- src/api/sign-in.js
+++ src/api/sign-in.js
@@ -1,8 +1,24 @@
-import { apiInstance } from './index.js';
+import { loginRequestApiInstance, apiInstance, getApiInstance } from './index.js';
+const loginRequestApi = loginRequestApiInstance();
 const api = apiInstance();
+const getApi = getApiInstance();
 
-function redirectToOauthLoginPage(oauthProvider) {
-    return api.get(`/oauth2/${oauthProvider}/login`);
+function getOauthLoginPage(oauthProvider) {
+    return getApi.get(`/oauth2/${oauthProvider}/login`);
 }
 
-export { redirectToOauthLoginPage };
+function loginWithOauth(oauthProvider, code) {
+    return loginRequestApi.get(`/oauth2/${oauthProvider}/callback`, {params: { code: code }});
+}
+
+function reissueAuthToken() {
+    return loginRequestApi.post(`/auth/token`);
+}
+
+function logout() {
+    const response = api.post(`/auth/logout`);
+    sessionStorage.removeItem('authToken');
+    return response;
+}
+
+export { getOauthLoginPage, loginWithOauth, reissueAuthToken, logout };
